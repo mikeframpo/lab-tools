362930f953c7ee1b5e4c396f03ff7c24623c815b
Mike Frampton <michael.frampton@pg.canterbury.ac.nz>
2013-06-21 15:24:18 +1200

    initial commit

---
+++ visa_simple.py
@@ -0,0 +1,143 @@
+
+
+# **** Design overview ****
+# 
+# Objective:
+#   to create a visa-like interface for scpi commands
+#   this will be used primarily for driving agilent instruments
+#
+# class Instrument:
+#   defines the normal interface for communicating with a device
+#   instanciated by passing a visa string into the constructor
+#   handles errors in a nice way
+#   provides a simple interface, write_cmd(), query_cmd()
+#
+# class Endpoint:
+#   abstract base class specifying the low level commands
+#
+# class SocketEndpoint:
+#   endpoint for a network socket.
+#
+# class SCPIError:
+#   
+
+#telnet interface stuff
+import telnetlib
+
+#endpoint base class stuff
+from abc import ABCMeta, abstractmethod
+
+#socketepoint stuff
+import socket
+
+class Endpoint:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def set_timeout(self, millis):
+        pass
+
+    @abstractmethod
+    def write_str(self, s):
+        pass
+
+    @abstractmethod
+    def read_str(self):
+        """Blocks until timeout reached, returns None if nothing was read."""
+        pass
+
+class SocketEndpoint:
+    
+    def __init__(self, address, port):
+        self.sock = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((address, port))
+
+    def set_timeout(self, millis):
+        pass
+
+    def write_str(self, s):
+        sent = self.write_line(s)
+        if sent == 0:
+            #TODO: raise an error
+            pass
+        return sent
+
+    def read_str(self):
+        return self.read_line()
+
+    def write_line(self, msg):
+        msg_with_line = msg + "\n"
+        totalsent = 0
+        while totalsent < len(msg_with_line):
+            sent = self.sock.send(msg_with_line[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent += sent
+        return totalsent
+
+    def read_line(self):
+        """reads a single line, or raises an error if a message was
+        not received within a timeout, i.e. if the user expected a response
+        from a non-response command."""
+        msg = self.sock.recv(100)
+        return msg
+
+Endpoint.register(SocketEndpoint)
+
+def visa_log(msg):
+    print("visa_simple: " + msg)
+
+VISA_USER_REQUEST = (1 << 6)
+VISA_COMMAND_ERROR = (1 << 5)
+VISA_EXEC_ERROR = (1 << 4)
+
+class Instrument:
+
+    #TODO: change the method definition to parse a SCPI string
+    def __init__(self, address, port):
+
+        self.endp = SocketEndpoint(address, port)
+        visa_log("Connection to device successfull")
+
+        self.endp.set_timeout(1000)
+        self.endp.write_str("*IDN?")
+        idn = self.endp.read_str()
+        if idn != None:
+            visa_log(idn)
+        else:
+            visa_log("Failed to read device IDN")
+
+        # enable device errors to be reported
+        # TODO: make this settable by passing in a device-config
+        self.endp.write_str("*ESE=" + 
+            str(VISA_USER_REQUEST |
+            VISA_COMMAND_ERROR |
+            VISA_EXEC_ERROR))
+
+    def put_cmd(self, cmd):
+        """Send the command to the device, do not attempt to read a result."""
+        self.endp.write_str("*CLS")
+        self.endp.write_str(cmd)
+        self.check_errors()
+
+    def query_cmd(self, cmd):
+        """Send the command to the device and then read and parse the result."""
+        put_cmd(cmd)
+        result = self.endp.read_str()
+        if result != None:
+            pass
+            #TODO: parse it, throw an error if nothing was returned
+        return result
+
+    def check_errors(self):
+        self.endp.write_str("*ESR?")
+        result = self.endp.read_str()
+        if result != None:
+            status = int(result)
+            #TODO: parse the errors, throw errors
+        #throw an error if nothing returned
+
+siggen = Instrument("132.181.52.71", 5024)
+siggen.put_cmd("NOTCOMMAND")
+
